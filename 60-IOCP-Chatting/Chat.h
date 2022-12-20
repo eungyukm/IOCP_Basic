3aad070bd55a908ffb82d58debb107ec5e304d57
eungyukm <eungyukm@gmail.com>
2022-12-20 09:32:29 +0900

    소스 코드 및 자료 업로드

---
+++ 60-IOCP-Chatting/Chat.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <iostream>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+const char serverIP[] = "127.0.0.1";
+//const char serverIP[] = "172.29.101.74";
+const int serverPort = 12314;
